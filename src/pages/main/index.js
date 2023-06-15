c86d848136579d39ca4fd98e56fdb5d23c18d023
Jaewon <grayhound0801@gmail.com>
2023-06-15 15:53:04 +0900

    이전버튼과 다음버튼 구현 완료

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,21 +2,18 @@ import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getIssue } from '../../reducer/issue'
 import Pagination from './components/Pagination'
-import { useParams } from 'react-router-dom' // 추가
+import { useParams } from 'react-router-dom'
 import ItemList from './components/ItemList'
+import styled from 'styled-components'
 
 const MainPage = () => {
 	const dispatch = useDispatch()
 	const [isLoading, setIsLoading] = useState(true)
 	const [limit, setLimit] = useState(10)
-	const { page } = useParams() // 추가
+	const { page } = useParams()
 
 	const res = useSelector(state => state.issue.issue)
 	console.log('index Main', res)
-
-	const pageCount = () => {
-		return Math.ceil(res.length / limit)
-	}
 
 	const handleLimitChange = event => {
 		const newLimit = parseInt(event.target.value, 10)
@@ -24,26 +21,66 @@ const MainPage = () => {
 	}
 
 	useEffect(() => {
-		dispatch(getIssue({ page: page || 1, limit })) // page가 정의되지 않은 경우 기본값 1 사용
+		dispatch(getIssue({ page: page || 1, limit }))
 	}, [page, limit, dispatch])
+
+	const totalItems = res.length // Calculate the total number of items based on the length of 'res'
+	const totalPages = Math.ceil(totalItems / limit)
+	const visiblePages = totalPages > 1 ? Math.min(totalPages, 8) : 1
+
+	const handlePageClick = page => {
+		dispatch(getIssue({ page, limit }))
+	}
 
 	return (
 		<>
-			{<ItemList data={res} />}
-			<select value={limit} onChange={handleLimitChange}>
-				<option value={10}>10</option>
-				<option value={20}>20</option>
-				<option value={30}>30</option>
-			</select>
-			{/* Inside the return statement of MainPage component */};
-			{/* {res.map(item => (
-			<ItemList data={item} />
-		))} */}
-			<Pagination limit={limit} pages={10} />
+			<ItemList data={res} />
+			<S.wrapper>
+				<S.optionWrapper>
+					<S.pageSelect value={limit} onChange={handleLimitChange}>
+						<option value={10}>10개</option>
+						<option value={20}>20개</option>
+						<option value={30}>30개</option>
+					</S.pageSelect>
+					{limit === 10 && (
+						<Pagination limit={limit} pages={10} onClick={handlePageClick} />
+					)}
+					{limit === 20 && (
+						<Pagination limit={limit} pages={10} onClick={handlePageClick} />
+					)}
+					{limit === 30 && (
+						<Pagination limit={limit} pages={8} onClick={handlePageClick} />
+					)}
+				</S.optionWrapper>
+			</S.wrapper>
 		</>
 	)
 }
+
 export default MainPage
+
+const wrapper = styled.div`
+	display: flex;
+
+	width: 100%;
+	position: fixed;
+	justify-content: center;
+	bottom: 20px;
+`
+
+const optionWrapper = styled.div`
+	display: flex;
+`
+
+const pageSelect = styled.select`
+	margin-right: 10px;
+`
+
+const S = {
+	wrapper,
+	optionWrapper,
+	pageSelect,
+}
 // import { useEffect, useState } from 'react'
 // import ItemList from './components/ItemList'
 // import { useDispatch, useSelector } from 'react-redux'
